bd981daf9f408b03ae5d458511e4c21654d854e2
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-07-03 14:36:09 -0300

    feat: princing rules

---
+++ src/Domain/PricingRules/PriceRule.cs
@@ -0,0 +1,30 @@
+﻿using Domain.ValueObjects;
+
+namespace Domain.PricingRules;
+
+
+public sealed class PriceRule
+{
+	public Percentage Modifier { get; private set; }
+
+	public PriceRule(Percentage percentage)
+	{
+		Modifier = percentage;
+	}
+
+	public static PriceRule FromDecimal(decimal modifier/*, markup range, date range*/)
+	{
+		ArgumentOutOfRangeException.ThrowIfNegative(modifier);
+
+		Percentage percentageModifier = Percentage.FromFraction(Math.Abs(modifier));
+
+		return new(percentageModifier);
+	}
+
+	public bool CanModify(Markup markup, DateTime now) { return false; }
+
+	public Price Modify(Price price)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/Domain/ValueObjects/Percentage.cs
@@ -0,0 +1,41 @@
+﻿namespace Domain.ValueObjects;
+
+public sealed class Percentage
+{
+	public decimal Value { get; private set; }
+
+	public Percentage(decimal value)
+	{
+		ArgumentOutOfRangeException.ThrowIfNegative(value);
+
+		Value = value;
+	}
+
+	public bool GreaterThan(Percentage other)
+		=> Value > other.Value;
+
+	public static Percentage FromDecimal(decimal percentage)
+		=> new(Math.Round(percentage, 2));
+
+	public static Percentage FromFraction(decimal fraction)
+	{
+		ArgumentOutOfRangeException.ThrowIfNegative(fraction);
+
+		return FromDecimal(fraction * 100);
+	}
+
+	public decimal ToFraction()
+	{
+		return Math.Round(Value / 100m, 2);
+	}
+
+	public decimal ApplyTo(decimal amount)
+	{
+		return amount * ToFraction();
+	}
+
+	public override string ToString()
+	{
+		return $"{Value}%";
+	}
+}
