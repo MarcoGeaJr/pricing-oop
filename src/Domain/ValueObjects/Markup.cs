186207ba93343e04c54a121e64bc0cfa4f7f0997
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-07-03 21:18:27 -0300

    feat: markup

---
+++ src/Domain/ValueObjects/Markup.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.ValueObjects;
+
+public sealed class Markup
+{
+	public Percentage ProfitMargin { get; }
+
+	public Markup(decimal margin)
+	{
+		ArgumentOutOfRangeException.ThrowIfNegative(margin);
+
+		ProfitMargin = Percentage.FromDecimal(margin);
+	}
+
+	public Price ApplyToCost(Price cost)
+	{
+		Price percentageOfCost = cost * ProfitMargin;
+
+		return cost + percentageOfCost;
+	}
+}
